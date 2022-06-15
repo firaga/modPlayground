1055901f692f2c6e9f3a631565a06cc2fc93312a
firaga <zhou.jichen@gmail.com>
2022-06-15 16:15:39 +0800

    binary测试

---
+++ binary/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+)
+
+func main() {
+	bs := make([]byte, 8)
+	binary.LittleEndian.PutUint64(bs, uint64(64))
+	fmt.Printf("%#v\n", bs)
+
+	i := binary.LittleEndian.Uint64(bs)
+	fmt.Println(i)
+}
