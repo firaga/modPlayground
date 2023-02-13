f6c841a3c39ce07da307f7487b08a9c60c5490f1
firaga <zhou.jichen@gmail.com>
2023-02-13 19:42:05 +0800

    rand源码

--- make/map/main.go
+++ make/map/main.go
@@ -4,7 +4,14 @@ import (
 	"fmt"
 )
 
+type H map[string]string
+
 func main() {
+	//a:=H{}
+	//a["code"] = "100"
+
+	//b:=map[string]string{}
+
 	m := make(map[string]int64, 53)
 	m["a"] = 111111111
 	m["b"] = 111111111
---
+++ openwechat/go.mod
@@ -0,0 +1,5 @@
+module owc
+
+go 1.17
+
+require github.com/eatmoreapple/openwechat v1.3.9 // indirect
